401b0fe5536b1f03e3677ad400971e7914f09d90
Dominique Youmans and Matt Taylor <dominique+matt@>
2020-02-26 11:23:40 -0800

    add instances of class Animal

--- Models/Program.cs
+++ Models/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using Wildlife.Model;
 
 namespace Wildlife
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+           Animal zebra = new Animal("Tony", "Zebra", "1/12/20", true, false, "Male");
+           Animal giraffe = new Animal("Geraldine", "Giraffe", "6/4/20", false, true, "Female");
+           Animal penguin = new Animal("Pauly", "Penguin", "2/2/20", true, true, "Male");
+           Animal shark = new Animal("Shirley", "Shark", "2/9/19", true, true, "Female");
+
+        List<Animal> Animals = new List<Animal>() {zebra, giraffe, penguin, shark};
+
+           
+
         }
     }
 }
